5d901f8360f6469e2817e24b9172e338ff1a22f5
Karthikan <55430090+kavim19@users.noreply.github.com>
2023-10-31 20:47:45 +0100

    Greenhouse kafka producer connector added with python script and yml file

---
+++ src/kafka/producers/greenhouse_gas_emissions/greenhouse_kafka_connect_producer.py
@@ -0,0 +1,26 @@
+from confluent_kafka import Producer
+import csv
+import os
+
+kafka_broker = os.environ.get("KAFKA_BROKER_ADDRESS")
+
+
+
+configuration = {
+    'bootstrap.servers': kafka_broker,
+    'client.id': 'greenhouse-kafka-connect-producer'
+}
+producer = Producer(configuration)
+
+
+while True:
+    try:
+        with open('data/asset_enteric-fermentation_emissions.csv', newline='') as file:
+            file_reader = csv.reader(file)
+            for line in file_reader:
+                data = ','.join(line)
+                producer.produce('greenhouse_topic', value=data.encode('utf-8'))
+
+        producer.flush()
+    finally:
+        producer.flush()
